c0a82756147fe6c08ac6e9de24db38200acbea8c
Mike North <michael.l.north@gmail.com>
2019-12-07 00:35:23 -0800

    generate sample CSVs from fixtures

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,7 +4,33 @@ import * as fs from "fs-extra";
 import * as cliui from "cliui";
 import chalk from "chalk";
 
-import { generateReport } from "../index";
+import { generateReport, GenerateReportOptions } from "../index";
+
+function printArgSummary(args: Partial<GenerateReportOptions>): void {
+  const ui = cliui({ width: 80 });
+
+  ui.div({ padding: [2, 0, 0, 0], text: "Ember-CLI Asset Size Report" });
+  ui.div({ text: "----------------------------------" });
+  ui.div({
+    padding: [0, 0, 2, 0],
+    text: chalk.dim("arguments received were as follows...")
+  });
+  ["build", "project", "out", "extra-js-file"].forEach(arg => {
+    const key = arg as keyof GenerateReportOptions;
+    ui.div(
+      {
+        text: arg,
+        width: 20
+      },
+      {
+        text: chalk.dim(JSON.stringify(args[key]))
+      }
+    );
+  });
+  ui.div();
+
+  console.log(ui.toString());
+}
 
 export async function main(): Promise<void> {
   const configPath = await findUp([".asset-report", ".asset-report.json"]);
@@ -19,29 +45,17 @@ export async function main(): Promise<void> {
       description: "path to the root of the ember-cli project"
     })
     .option("build", { type: "boolean", default: true })
-    .option("extra-js-file", { array: true })
+    .option("extra-js-file", { array: true, default: [] })
     .config(config)
     .pkgConf("asset-report")
     .parse(process.argv);
+  const opts: Partial<GenerateReportOptions> = {
+    build: prog.build,
+    extraJsFiles: prog["extra-js-file"],
+    project: prog.project,
+    out: prog.out
+  };
+  printArgSummary(opts);
 
-  const ui = cliui({ width: 80 });
-
-  ui.div({ text: "Arguments" });
-  ui.div({ text: "-----------------" });
-  ["build", "project", "out", "extra-js-file"].forEach(arg => {
-    ui.div(
-      {
-        text: arg,
-        width: 20
-      },
-      {
-        text: chalk.dim(prog[arg] as any)
-      }
-    );
-  });
-
-  console.log(ui.toString());
-  // const argv;
-
-  // await generateReport({});
+  await generateReport(opts);
 }
