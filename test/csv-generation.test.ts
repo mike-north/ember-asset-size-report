a05f618060411987536667bf9866192cee346dd5
Mike North <michael.l.north@gmail.com>
2019-12-06 16:27:48 -0800

    generate sample CSV in /samples folder

--- test/csv-generation.test.ts
+++ test/csv-generation.test.ts
@@ -1,4 +1,11 @@
 import { generateReport, Project as EmberProject } from "../src";
+import { join } from "path";
+import * as pkgUp from "pkg-up";
+const PKG_JSON_PATH = pkgUp.sync();
+if (!PKG_JSON_PATH)
+  throw new Error(
+    "Tests can only be run in a folder (or child of folder) containing a package.json"
+  );
 
 QUnit.module("acceptance tests", _hooks => {
   QUnit.test("exported symbols are functions", assert => {
@@ -11,7 +18,10 @@ QUnit.module("acceptance tests", _hooks => {
 
   QUnit.test("create + build", async assert => {
     const project = await EmberProject.emberNewInTemp("my-app");
-    await generateReport(project.path);
+    await generateReport(
+      project.path,
+      join(PKG_JSON_PATH, "..", "samples", "module-size-report.csv")
+    );
     assert.ok(true);
   });
 });
